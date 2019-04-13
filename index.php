b80562d083c1866d436f7eb5dbc77e6c6220cf9f
julienbltt <julien.blt@yahoo.com>
2019-04-13 16:30:03 +0200

    Inclusion of templates in index.php

--- index.php
+++ index.php
@@ -2,29 +2,14 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" href="design\style_home.css">
+        <link rel="stylesheet" type="text/css" href="design\style.css">
 		<title>DIASPORA</title>
 	</head>
 	
 	<body>
 		<div id="bloc_page">
-			<header>
-				<div id="titre"> 
-					<img src="design\img\BRAND\logo_titre.png" alt="Logo de la marque DIASPORA" />	
-				</div>
-				
-				<div id="logo"> 
-					<img src="design\img\BRAND\logo.png" alt="Logo d'une bousole" /> 
-				</div> 
-
-				<nav>
-					<ul>
-						<li><a href="#">VETEMENTS</a></li>
-						<li><a href="#">ACCESSOIRES</a></li>
-						<li><a href="#">ASSISTANCE</a></li>
-					</ul>
-				</nav>
-			</header>
+			
+			<?php include("template/header.php"); ?>
 
 			<div id="conteneur_articles"> <!-- Reeplce all the box for the image -->
 				<div class="article"></div> <!-- <img src="disign\img\articles\pull_bleu.png"> -->
@@ -33,15 +18,8 @@
 				<div class="article"></div> <!-- <img src="disign\img\articles\pull_vert.png"> -->
 			</div>
 
-			<footer>
-				<p>Home > </p>
-				<p>
-					Contacts : Mail <a href="mailto:nommail@truc.com">nommail@truc.com</a> - Tel : 0000000000 | <a href="mailto:nommail@truc.com">SUPPORT</a> <br/>
-					Copyright © 2019 DIASPORA. Tout droit reservés. <br />
-					<a href="#">Engagement confidentalité</a> | <a href="#">Utilisation des cookies</a> | <a href="#">Condition d'utilisation</a> | <a href="#">Vente rembourcement</a> | 
-					<a href="#"> Mention Legal</a> | <a href="#">Plant du site</a>
-				</p>
-			</footer>
+			<?php include("template/footer.php"); ?>
+
 		</div>
 	</body>
 </html>
