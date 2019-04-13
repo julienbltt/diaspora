7c482af8e6e8bae00dcb6a70e76cf7dcb96f9d5d
julienbltt <julien.blt@yahoo.com>
2019-04-13 12:18:29 +0200

    Adding html code in index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+		<title>DIASPORA</title>
+	</head>
+	
+	<body>
+		<div id="bloc_page">
+			<header>
+				<div id="titre"> 
+					<img src="img\logo_titre.png">	
+				</div>
+				
+				<div id="logo"> 
+					<img src="img\logo.png"> 
+				</div> 
+
+				<nav>
+					<ul>
+						<li><a href="#">VETEMENTS</a></li>
+						<li><a href="#">ACCESSOIRES</a></li>
+						<li><a href="#">ASSISTANCE</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<section id="articles">
+				<div class="article"><img src="articles\pull_bleu.png"></div>
+				<div class="article"><img src="articles\pull_jaune.png"></div>
+				<div class="article"><img src="articles\pull_rose.png"></div>
+				<div class="article"><img src="articles\pull_vert.png"></div>
+			</section>
+
+			<footer>
+				<p>Footer</p>
+			</footer>
+		</div>
+	</body>
+</html>
