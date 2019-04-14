32d343f5f507081cc7c5ab6f0d270cd0a5b21364
julienbltt <julien.blt@yahoo.com>
2019-04-14 13:09:30 +0200

    Adding casquettes.php page (but no css)

---
+++ accessoires/casquettes.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	    <link rel="stylesheet" type="text/css" href="/espace_travail/DIASPORA/design/style.css" />
+		<title>DIASPORA - CASQUETTES</title>
+	</head>
+	
+	<body>
+		<div id="block_page">
+			
+			<?php include("C:/wamp/www/espace_travail/DIASPORA/template/header.php"); ?>
+
+			<!--------------------- Articles -------------------------------------- -->
+			<div id="articles_casquettes">
+				<div class="article_casquette_img">
+					<img src="../design/img/articles/[name_of_img_article]">
+				</div>
+				<div class="article_info">
+					<p>[Nom] <br /> [Prix]</p>
+					<div>
+						<img src="../design/img/couleurs/[name_of_color]" alt="couleur" class="couleur">
+						<img src="../design/img/couleurs/[name_of_color]" alt="couleur" class="couleur">						
+						<img src="../design/img/couleurs/[name_of_color]" alt="couleur" class="couleur">
+					</div>
+				</div>
+			</div>
+			<!-- ------------------------------------------------------------------ -->
+			<!-- ------------------- Articles (reverse) --------------------------- -->
+			<div id="articles_casquettes_reverse">
+				<div class="article_casquette_img">
+					<img src="../design/img/articles/[name_of_img_article]">
+				</div>
+				<div class="article_info">
+					<p>[Nom] <br /> [Prix]</p>
+					<div class="dot_color">
+						<img src="../design/img/couleurs/[name_of_color]" alt="couleur" class="couleur">
+						<img src="../design/img/couleurs/[name_of_color]" alt="couleur" class="couleur">
+						<img src="../design/img/couleurs/[name_of_color]" alt="couleur" class="couleur">
+					</div>
+				</div>
+			</div>
+			<!-- ------------------------------------------------------------------ -->
+			
+			<!-- Here increments new articles -->
+
+			<!-- ------------------------------------------------------------------ -->
+			<p>Home > Vetements > Casquettes </p>
+			<?php include("C:/wamp/www/espace_travail/DIASPORA/template/footer.php"); ?>
+
+		</div>
+	</body>
+</html>
